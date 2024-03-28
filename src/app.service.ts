03906cd025f6f5e9b2fce7db01b93d9f954fe203
luis <lgonzalez@doto.com.mx>
2024-03-27 19:37:01 -0600

    Se elimina el log de Raiz

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    console.log('se ejecuto la raiz health')
+    //console.log('se ejecuto la raiz health')
     return 'Hello World!';
   }
 }
